fe71bf2d09575da7ae73477c89b0904d6a03f606
Hesenli-Hacaga <hacaga125+1@gmail.com>
2023-03-08 01:12:40 +0400

    Folder Structures

---
+++ src/components/DragDrop/index.jsx
@@ -0,0 +1,10 @@
+import { images } from "../../constants"
+
+const DragDrop = () => {
+    return (
+        <>
+            <div></div>
+            <div></div>
+        </>
+    )
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import { DndProvider } from 'react-dnd'
+import { HTML5Backend } from 'react-dnd-html5-backend'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  <DndProvider backend={HTML5Backend}>
     <App />
-  </React.StrictMode>,
+  </DndProvider>,
 )
